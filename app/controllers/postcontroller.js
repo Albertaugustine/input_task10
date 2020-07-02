d308b2c87d522647228e2f8cdaac00437c0deebf
Albertaugustine <yesuarielalbert@gmail.com>
2020-07-02 17:27:24 +0530

    Add files via upload

---
+++ app/controllers/postcontroller.js
@@ -0,0 +1,8 @@
+
+app.controller('myCtrl',['$scope',function($scope){
+    $scope.Isee=false;
+    $scope.trigr=function(){
+        $scope.Isee=true;        
+
+    };
+}]);
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+<script src="app/route.js"></script>
+<script src="app\controllers\postcontroller.js"></script>
+</head>
+<body ng-app="myApp">
+
+    <div class="jumbotron text-center">
+        <h2>WELCOME</h2>
+    
+    </div>
+    <a href="#/!"></a>    
+    <div ng-view></div>
+
+</body>
+</html>
